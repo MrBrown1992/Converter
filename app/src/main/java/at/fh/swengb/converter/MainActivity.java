bdd791603e2b271a4fb31d6749d24a100b784292
MrBrown1992 <nikolaus.spiess@edu.fh-joanneum.at>
2016-11-17 11:06:57 +0100

    Initial commit

---
+++ app/src/main/java/at/fh/swengb/converter/MainActivity.java
@@ -0,0 +1,53 @@
+package at.fh.swengb.converter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText inputText;
+    private TextView resultView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputText = (EditText) findViewById(R.id.inputText);
+        resultView = (TextView) findViewById(R.id.resultView);
+
+    }
+
+
+    public void convert(View view) {
+
+        String num1String = inputText.getText().toString();
+
+        double num1 = 0;
+        double num2 = 1.073741824;
+        double result = 0;
+
+        try {
+            num1 = Double.parseDouble(num1String);
+
+            result = num1 / num2;
+            resultView.setText("" + result);
+
+        } catch (NumberFormatException e) {
+            resultView.setText("WRONG INPUT!!!!");
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void showAbout(View view) {
+        Intent intent = new Intent(this, Show_About.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/at/fh/swengb/converter/Show_About.java
@@ -0,0 +1,17 @@
+package at.fh.swengb.converter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Show_About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show__about);
+
+
+    }
+}
